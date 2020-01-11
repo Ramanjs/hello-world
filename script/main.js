cad599b3eb24143d6879054d3d7490b058dc08af
Ramanjs <raman.s2662@gmail.com>
2020-01-11 21:01:22 +0530

    added javascript

---
+++ script/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	let mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/w3schools.jpg'){
+		myImage.setAttribute('src','images/download.png');
+	} else {
+		myImage.setAttribute('src','images/w3schools.jpg');
+	}
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  	let myName = prompt('Please enter your name.');
+  	if(!myName || myName === null) {
+ 		setUserName();
+ 	} else {
+ 		localStorage.setItem('name', myName);
+ 		myHeading.textContent = 'Mozilla is cool, ' + myName;
+	}
+	if(!localStorage.getItem('name')) {
+		setUserName();
+	} else {
+  		let storedName = localStorage.getItem('name');
+  		myHeading.textContent = 'Mozilla is cool, ' + storedName;
+ 	}
+}
+myButton.onclick = function() {
+  setUserName();
+}
